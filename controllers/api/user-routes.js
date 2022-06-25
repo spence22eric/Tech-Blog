aebaaabb5833820a7e765046f187b5983f1902d3
Eric Spencer <spencer22eric@gmail.com>
2022-06-25 11:14:03 -0400

    add put user route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// create user
 router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
@@ -14,6 +15,29 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
     })
 })
+
+// PUT route
+router.put('/:id', (req, res) => {
+    // expects username and password
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' })
+            return;
+        }
+        res.json(dbUserData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// DELETE route
 
 
 module.exports = router;
